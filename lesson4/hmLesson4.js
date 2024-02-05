126b89742bb579ee90ae41abd41ed1152037957f
nonobromo <noamab22@gmail.com>
2024-02-05 22:23:22 +0200

    homework done

---
+++ lesson4/hmLesson4.js
@@ -0,0 +1,36 @@
+function replace() {
+    const num1 = document.getElementById("str1");
+    const num2 = document.getElementById("str2");
+
+    change1 = num1.value;
+    change2 = num2.value;
+
+    num1.value = change2;
+    num2.value = change1;
+}
+
+function salerycalcultor() {
+    const user = document.getElementById("user").value;
+    let paycheck = +document.getElementById("salery").value;
+    let result = document.getElementById("output4");
+
+    if (paycheck <= +6000) {
+        paycheck = paycheck + paycheck / +100 * +10;
+    } else {
+        paycheck = paycheck + paycheck / +100 * +5;
+    }
+    result.innerHTML = `${user} salery is ${paycheck}`;
+}
+
+function check() {
+    const age = document.getElementById("age").value;
+    const height = document.getElementById("number").value;
+    const check = document.getElementById("output5");
+
+    if (age >= 14 || age >= 21 && height > 182) {
+        check.innerHTML = "you are qualified";
+    } else {
+        check.innerHTML = "you are not qualified";
+    }
+
+}
