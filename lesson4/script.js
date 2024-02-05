e1e713521bf97ce11265b087b40331534b348cc4
nonobromo <noamab22@gmail.com>
2024-02-05 18:47:13 +0200

    added

---
+++ lesson4/script.js
@@ -0,0 +1,40 @@
+let counter = 0;
+function increase() {
+    // הגדלת משתנה ב 1
+    counter++;
+    //הכנסת ערך ב HTML
+    document.getElementById("number").innerHTML = counter;
+    //הגדלת גודל הטקסט
+    document.getElementById("number").style.fontSize = counter + "em";
+}
+
+function decrease() {
+    // הקטנת משתנה ב 1
+    counter--;
+    // הכנסת ערך ב HTML
+    document.getElementById("number").innerHTML = counter;
+    // הקטנת גודל הטקסט בהתאם לערך של המספר
+    document.getElementById("number").style.fontSize = counter + "em";
+}
+
+function bestCity() {
+    const city = document.getElementById("city").value;
+    if (city == "נתניה") {
+        document.getElementById("output1").innerHTML = "טעות!";
+    } else if (city == "חיפה") {
+        document.getElementById("output1").innerHTML = "נכון!";
+    } else if (city == "פתח תקווה") {
+        document.getElementById("output1").innerHTML = "טעות!";
+    } else if (city == "הרצליה") {
+        document.getElementById("output1").innerHTML = "טעות!";
+    } else if (city == "חולון") {
+        document.getElementById("output1").innerHTML = "טעות!";
+    } else if (city == "אשלקון") {
+        document.getElementById("output1").innerHTML = "טעות!";
+    } else if (city == "חדרה") {
+        document.getElementById("output1").innerHTML = "טעות!";
+    } else if (city == "ראשון לציון") {
+        document.getElementById("output1").innerHTML = "טעות!";
+    }
+
+}
