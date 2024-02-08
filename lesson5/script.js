870f8fff5968cf54ead1ffb2e5fa0e8df870696d
nonobromo <noamab22@gmail.com>
2024-02-08 20:54:44 +0200

    changes

--- lesson5/script.js
+++ lesson5/script.js
@@ -13,3 +13,20 @@ function sum(num1, num2, num3) {
 function calcVat(num) {
     return num * 1.17;
 }
+
+function welcome(greet, fname, lname) {
+    return greet + fname + lname;
+}
+
+function calcAvg(num1, num2, num3, num4) {
+
+    //Method one using order of operations
+    return (num1 + num2 + num3 + num4) / 4;
+
+    //method Two saving the sum of the four numbers and then dividing it by 4
+
+    // let sum = num1 + num2 + num3 + num4;
+    // return sum /4;
+
+}
+
