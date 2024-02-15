b2cd83e597061e31c1bfa3fad55a6ce197d99e00
nonobromo <noamab22@gmail.com>
2024-02-15 17:34:35 +0200

    changes

--- lesson6/script.js
+++ lesson6/script.js
@@ -7,8 +7,8 @@ function task1() {
         i += 2;
     }
 
-    document.getElementById("output1").innerHTML = res;
-    document.getElementById("btn").remove();
+    document.getElementById('output1').innerHTML = res;
+    document.getElementById('btn').remove();
 }
 
 function task2(elem) {
@@ -18,7 +18,6 @@ function task2(elem) {
     let i = 2;
 
     while (i < num) {
-
         if (num % i === 0) {
             isPrime = false;
             break;
@@ -27,19 +26,19 @@ function task2(elem) {
         i++;
     }
 
-    const output = document.getElementById("output2");
+    const output = document.getElementById('output2');
 
     if (isPrime) {
         output.innerHTML = 'ראשוני';
-        output.style.color = "blue";
+        output.style.color = 'blue';
     } else {
         output.innerHTML = 'פריק';
-        output.style.color = "green";
+        output.style.color = 'green';
     }
 }
 
 function task3() {
-    const num = +document.getElementById("num1").value;
+    const num = +document.getElementById('num1').value;
     let result = '';
 
     let i = 1;
@@ -50,45 +49,72 @@ function task3() {
         i++;
     }
 
-    document.getElementById("output3").innerHTML = result;
+    document.getElementById('output3').innerHTML = result;
 }
 
 function task4() {
-    const num1 = document.getElementById("num3").value;
-    const num2 = document.getElementById("num4").value;
+    let num1 = document.getElementById('num3').value;
+    let num2 = document.getElementById('num4').value;
 
     let result = '';
-    let i = +num1;
 
-    while (i < num2) {
-        document.getElementById("output4").innerHTML = result
-        i++;
-        result += i;
+    while (num1 <= num2) {
+        result += num1 + ',';
+        num1++;
     }
+    document.getElementById('output4').innerHTML = result;
 }
 
 function task5(el) {
     const num = el.value;
-
     let i = 1;
+    let result = '';
+
     while (i <= num) {
         if (num % i === 0) {
-            console.log(i);
+            result += i += ' ';
         }
         i++;
     }
+    document.getElementById('output5').innerHTML = result += ' ';
 }
 
 function task6() {
-    const num = document.getElementById("num6").value;
+    const num = document.getElementById('num6').value;
 
     let result = 1;
     let i = 1;
-
-
+    let calc = '';
     while (i <= num) {
+        calc += i + '*';
         result *= i;
         i++;
     }
-    console.log(result)
+    document.getElementById('output6').innerHTML = `${calc} = ${result}`;
+}
+
+function task7() {
+    const num = document.getElementById('num7').value;
+
+    let result = '';
+    let i = 1;
+
+    while (i <= num) {
+        result += '*';
+        i++;
+    }
+    document.getElementById('output7').innerHTML = result;
+}
+
+function task8() {
+    const num = document.getElementById('num8').value;
+
+    let result = '';
+    let i = 1;
+
+    while (i <= num) {
+        result += '*' + '<br>';
+        i++;
+    }
+    document.getElementById('output8').innerHTML = result;
 }
